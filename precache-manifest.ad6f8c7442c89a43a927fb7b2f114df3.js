e4785726254ce89bacbb1dbeb3ddd9822465388b
Felipe Souza <lms.felipe@gmail.com>
2018-11-12 17:30:00 -0200

    Updates

--- precache-manifest.ad6f8c7442c89a43a927fb7b2f114df3.js
+++ precache-manifest.ad6f8c7442c89a43a927fb7b2f114df3.js
@@ -4,19 +4,19 @@ self.__precacheManifest = [
     "url": "/cep-search/static/js/runtime~main.8df8682f.js"
   },
   {
-    "revision": "253d62edcc46405a8090",
-    "url": "/cep-search/static/js/main.253d62ed.chunk.js"
+    "revision": "fee083ce31579c4088d6",
+    "url": "/cep-search/static/js/main.fee083ce.chunk.js"
   },
   {
     "revision": "9c749dc9f7e8f23bbf02",
     "url": "/cep-search/static/js/1.9c749dc9.chunk.js"
   },
   {
-    "revision": "253d62edcc46405a8090",
+    "revision": "fee083ce31579c4088d6",
     "url": "/cep-search/static/css/main.9eca3ce4.chunk.css"
   },
   {
-    "revision": "fcbb32dcc1b44021f68c6fe6bb426626",
+    "revision": "316fe5e3c9995a5632635a1495a9695f",
     "url": "/cep-search/index.html"
   }
 ];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/cep-search/precache-manifest.a6ef639f518703b86ad9b4d84dff3d50.js"
+  "/cep-search/precache-manifest.ad6f8c7442c89a43a927fb7b2f114df3.js"
 );
 
 workbox.clientsClaim();
