90ce0f99ad14cad0e7f167f98b3fd1432ad98ad6
Felipe Souza <lms.felipe@gmail.com>
2018-11-12 14:26:19 -0200

    Updates

---
+++ precache-manifest.9c7b34b09682db6743ba3a51b7fe0cf0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "8df8682f5d81e6934a1a",
+    "url": "/cep-search/static/js/runtime~main.8df8682f.js"
+  },
+  {
+    "revision": "8808889ac8de09185602",
+    "url": "/cep-search/static/js/main.8808889a.chunk.js"
+  },
+  {
+    "revision": "9c749dc9f7e8f23bbf02",
+    "url": "/cep-search/static/js/1.9c749dc9.chunk.js"
+  },
+  {
+    "revision": "8808889ac8de09185602",
+    "url": "/cep-search/static/css/main.9eca3ce4.chunk.css"
+  },
+  {
+    "revision": "42dd1f9b919411f8a7068c5bf31b1cc3",
+    "url": "/cep-search/index.html"
+  }
+];
---
+++ service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+importScripts(
+  "/cep-search/precache-manifest.9c7b34b09682db6743ba3a51b7fe0cf0.js"
+);
+
+workbox.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute("/cep-search/index.html", {
+  
+  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+});
