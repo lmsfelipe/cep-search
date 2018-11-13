cbb22d3119bdf0d3d92422308de7f19645754890
Felipe Souza <lms.felipe@gmail.com>
2018-11-13 12:02:36 -0200

    Fix button

--- src/components/button/index.js
+++ src/components/button/index.js
@@ -3,7 +3,7 @@ import PropTypes from 'prop-types';
 
 import './button.css';
 
-const Button = ({ children }) => <div className="button">{children}</div>
+const Button = ({ children, ...rest }) => <div className="button" {...rest}>{children}</div>
 
 Button.propTypes = {
   children: PropTypes.node.isRequired
